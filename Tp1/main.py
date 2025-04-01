16194580d1601f570e63776fc83836fc52922692
nancy-ni <nni@fi.uba.ar>
2025-04-01 17:37:05 -0300

    Se agrega funcion para contar las apariciones aparte

--- Tp1/main.py
+++ Tp1/main.py
@@ -1,17 +1,21 @@
+def contar_apariciones(intervalos):
+    intervalos_todos = {}
+    for j in range(len(intervalos)):
+        t_i, e_i = intervalos[j][0], intervalos[j][1]
+        if (t_i,e_i) not in intervalos_todos:
+            intervalos_todos[(t_i,e_i)] = 0
+        intervalos_todos[(t_i,e_i)] += 1
+    return intervalos_todos
+
+
 def comparar_datos(intervalos, transacciones):
     coincidencias = []
-    intervalos_todos = {}
+    intervalos_todos = contar_apariciones(intervalos)
 
     for i in range(len(transacciones)):
         s_i = transacciones[i]
         intervalo_i = ()
         min_fin = float('inf')
-
-        for j in range(len(intervalos)):
-            t_i, e_i = intervalos[j][0], intervalos[j][1]
-            if (t_i,e_i) not in intervalos_todos:
-                intervalos_todos[(t_i,e_i)] = 0
-            intervalos_todos[(t_i,e_i)] += 1
 
         for j in range(len(intervalos)):
             t_i, e_i = intervalos[j][0], intervalos[j][1]
@@ -35,13 +39,13 @@ def main():
     intervalos = []
     transacciones = []
 
-    for i in range(n):
+    for _ in range(n):
         linea = input()
         linea = linea.strip().split(",")
         t_i, e_i = int(linea[0]), int(linea[1])
         intervalos.append((t_i, e_i))
     
-    for i in range(n):
+    for _ in range(n):
         linea = input()
         linea = linea.strip()
         s_i = int(linea)
